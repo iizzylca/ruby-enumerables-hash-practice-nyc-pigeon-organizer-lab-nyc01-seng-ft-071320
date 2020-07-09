3569b35c19ede2ecbc395f3877a0813ca43ec962
Israel Canessa <israellcanessa@gmail.com>
2020-07-09 15:20:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-<<<<<<< HEAD
   final_results = data.each_with_object({}) do |(key, value), final_array |
     value.each do |last_key, names|
       names.each do |name|
@@ -17,7 +16,4 @@ def nyc_pigeon_organizer(data)
     end
   end
   #binding.pry
-=======
-  binding.pry
->>>>>>> 5bb9be70cc48a36fa157cd1ae3f7b93fd56e46ad
-end
+
